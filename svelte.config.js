87ef7f4e9ae7b6e42dc2da32649bf6eb776f0924
oceanbao <oceanbao@outlook.com>
2023-07-27 17:46:23 +0800

    fix: adapter config v1

--- svelte.config.js
+++ svelte.config.js
@@ -32,7 +32,6 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			routes: {
-				include: ['/*'],
 				exclude: ['/src/posts/*']
 			}
 		})
